263a514bbc171f17bd760fa41ca151f011d96993
awais-ansari <awaisansari1502@outlook.com>
2025-03-14 12:32:48 +0530

    added user schema

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accountType: {
+    type: String,
+    required: true,
+    enum: ["Admin", "Instructor", "Student"],
+  },
+
+  additionalDetails: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Profile",
+  },
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+  image: {
+    type: String,
+    required: true,
+  },
+
+  courseProgress: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CourseProgress",
+    },
+  ],
+});
+
+mongoose.exports = mongoose.model("User", userSchema);
